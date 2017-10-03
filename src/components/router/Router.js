69c0fe5a0eae791a9104d581fb46b63a989f4cb8
Ivan Uy <ivan@dnamicro.com>
2017-10-03 16:50:01 +0800

    Sync app state with browser history

--- src/components/router/Router.js
+++ src/components/router/Router.js
@@ -23,6 +23,12 @@ export class Router extends Component {
     }
   }
 
+  componentDidMount() {
+    window.onpopstate = () => {
+      this.setState({route: getCurrentPath()})
+    }
+  }
+
   render() {
     return (
       <div>
