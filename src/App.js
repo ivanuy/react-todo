3719a56173120d415094be3ef3524e28b64502c4
Ivan Uy <ivan@dnamicro.com>
2017-10-03 15:59:13 +0800

    Add footer with routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { TodoForm, TodoList } from './components/todo';
+import { TodoForm, TodoList, Footer } from './components/todo';
 import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo} from './lib/todoHelpers'
 import {pipe, partial} from './lib/utils'
 class App extends Component {
@@ -73,6 +73,7 @@ class App extends Component {
             handleSubmit={submitHandler}/>
 
           <TodoList handleRemove={this.handleRemove} handleToggle={this.handleToggle} todos={this.state.todos}/>
+          <Footer />
         </div>
       </div>
     );
