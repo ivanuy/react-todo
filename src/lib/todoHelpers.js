231ff85e3f656afdd576625a9dd1901b4b972361
Ivan Uy <ivan@dnamicro.com>
2017-10-04 11:20:45 +0800

    Add document code blocks

--- src/lib/todoHelpers.js
+++ src/lib/todoHelpers.js
@@ -1,11 +1,38 @@
+/**
+ * Add new todo
+ * 
+ * @param {array} list 
+ * @param {object} item 
+ * @return {array} new list
+ */
 export const addTodo = (list, item) => list.concat(item)
 
 export const generateId = () => Math.floor(Math.random() * 1000000)
 
+/**
+ * Find todo item in the list
+ * 
+ * @param {number} id single item
+ * @param {array} list todo list
+ * @return {object} a value in the array that matches the id
+ */
 export const findById = (id, list) => list.find(item => item.id === id)
 
+/**
+ * Update isCompleted value inside the todo and return a new updated todo
+ * 
+ * @param {object} todo todo item
+ * @return {object} updated item
+ */
 export const toggleTodo = (todo) => ({...todo, isCompleted: !todo.isCompleted})
 
+/**
+ * Update a todo inside the todo list/array and return a new updated array
+ * 
+ * @param {array} list 
+ * @param {object} updated 
+ * @return {array} new array with updated todo item
+ */
 export const updateTodo = (list, updated) => {
   const updatedIndex = list.findIndex(item => item.id === updated.id)
   return [
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,25 @@
+/**
+ * Partially add any argument(closure)
+ * 
+ * @param {function} fn 
+ * @param {*} args accept any multiple args
+ * @return {function} with partially added data
+ */
 export const partial = (fn, ...args) => fn.bind(null, ...args)
 
+/**
+ * Nest two functions
+ * 
+ * @param {function} fn1 
+ * @param {function} fn2
+ * @return {function} inner function that accepts multiple args
+ */
 const _pipe = (fn1, fn2) => (...args) => fn2(fn1(...args))
 
+/**
+ * Handles multiple function and return a single function
+ * 
+ * @param {function} fns accept multiple functions
+ * @return {function} reduce to a single function
+ */
 export const pipe = (...fns) => fns.reduce(_pipe)
